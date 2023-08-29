e628be69116191dcb250984d82d8d148ec1d4828
julifmontoya <julifmontoya@hotmail.com>
2023-08-29 12:40:04 -0500

    updatePermision

--- post/views.py
+++ post/views.py
@@ -47,14 +47,14 @@ class PostDetailProv(APIView):
     def get(self, request, id):
         post = get_object_or_404(Post, id=id)
         user = self.request.user
-        self.has_permission(post.user.id, user)
+        self.has_permission(post.user, user)
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
     def put(self, request, id):
         post = get_object_or_404(Post, id=id)
         user= self.request.user
-        if has_permission(post.user.id, user):
+        if has_permission(post.user, user):
             serializer = PostSerializer(post, data=request.data)
             if serializer.is_valid():
                 serializer.save()
@@ -64,8 +64,8 @@ class PostDetailProv(APIView):
         else:
             return Response({'error': 'Access denied'}, status=status.HTTP_403_FORBIDDEN)
         
-    def has_permission(self, provider_user_id, user):
-        if provider_user_id != user.id and not user.is_superuser:
+    def has_permission(self, provider_user, user):
+        if provider_user != user and not user.is_superuser:
             raise PermissionDenied()
 
 
